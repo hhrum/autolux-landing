5ed5057fdd7f19c15670ca3b2df9c74e9dcd08e9
hhrum <denis.smit72@gmail.com>
2026-08-12 13:31:40 +0500

    config fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,15 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
 
 // https://astro.build/config
 // GitHub Pages: https://docs.astro.build/en/guides/deploy/github/
 export default defineConfig({
-  // site: 'https://hhrum.github.io',
-  // base: '/autolux-landing',
+  site: 'https://hhrum.github.io',
+  base: '/autolux-landing',
   vite: {
     plugins: [tailwindcss()],
   },
-
   integrations: [react()],
 });
